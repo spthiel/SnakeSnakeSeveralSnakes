eee15d46f0f32a9ceddbefc8aedcb9a95d5591e9
spthiel <sp4rklingskull@gmail.com>
2022-10-10 22:35:33 +0200

    [FIX] Fix indentation of Test.java because it's embarassing

--- src/me/elspeth/Test.java
+++ src/me/elspeth/Test.java
@@ -9,35 +9,35 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-Server server = new Server();
-server.registerPacket(TestPackage.class);
-server.open();
-
-Client client = new Client();
-client.registerPacket(TestPackage.class);
-client.open();
+		Server server = new Server();
+		server.registerPacket(TestPackage.class);
+		server.open();
 		
-Client client2 = new Client();
-client2.registerPacket(TestPackage.class);
-client2.open();
-
-TestPackage toServer = new TestPackage("This package is sent to the server");
-TestPackage toClient = new TestPackage("This package is sent to the client");
-TestPackage reply = new TestPackage("This is a reply");
-
-server.on(TestPackage.class, (packet) -> {
-	System.out.println("Server: " + packet);
-	server.send(packet.sender, reply);
-});
-client.on(TestPackage.class, (packet) -> {
-	System.out.println("Client: " + packet);
-});
-client2.on(TestPackage.class, (packet) -> {
-	System.out.println("Client2: " + packet);
-});
-
-client.send(toServer);
-server.send(toClient);
+		Client client = new Client();
+		client.registerPacket(TestPackage.class);
+		client.open();
+		
+		Client client2 = new Client();
+		client2.registerPacket(TestPackage.class);
+		client2.open();
+		
+		TestPackage toServer = new TestPackage("This package is sent to the server");
+		TestPackage toClient = new TestPackage("This package is sent to the client");
+		TestPackage reply    = new TestPackage("This is a reply");
+		
+		server.on(TestPackage.class, (packet) -> {
+			System.out.println("Server: " + packet);
+			server.send(packet.sender, reply);
+		});
+		client.on(TestPackage.class, (packet) -> {
+			System.out.println("Client: " + packet);
+		});
+		client2.on(TestPackage.class, (packet) -> {
+			System.out.println("Client2: " + packet);
+		});
+		
+		client.send(toServer);
+		server.send(toClient);
 	}
 	
 }
